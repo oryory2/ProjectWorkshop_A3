a010edd67f0080a6e70afd06812eeb11d7e133fe
Or izhaky <oryory2@gmail.com>
2022-05-06 16:03:39 +0300

    checkPull

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("gg");
     }
 }
