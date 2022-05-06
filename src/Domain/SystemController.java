612b3b3c55fa3cf0a020e0499d02f6ac7568beda
Or izhaky <oryory2@gmail.com>
2022-05-06 20:26:52 +0300

    UI and systemController Funcs

--- src/Domain/SystemController.java
+++ src/Domain/SystemController.java
@@ -2,5 +2,23 @@ package Domain;
 
 public class SystemController
 {
-    private int age;
+    public SystemController()
+    {
+
+    }
+
+    public void loginUser(String userID, String password)
+    {
+
+    }
+
+    public void addReferee(String name, String qualification, String leagueId)
+    {
+
+    }
+
+    public void assignGame(String gameId, String teamOne, String teamTwo, String leagueIdOfTheGame, String dateAndTime, String referees, String field)
+    {
+
+    }
 }
