8eca76a95855f7cd608bf51324bb246c77c4d351
elbam <elbamit@hotmail.com>
2022-05-07 10:18:23 +0300

    Created DB_connector class and the connect/disconnect function

--- src/DataAccess/DB_connector.java
+++ src/DataAccess/DB_connector.java
@@ -1,2 +1,31 @@
-package DataAccess;public class DB_connector {
+package DataAccess;
+import java.sql.*;
+
+public class DB_connector {
+
+    /**
+     * Connect to a sample database
+     */
+    public static Connection connect() {
+        Connection conn = null;
+        String url = "jdbc:sqlite:db_hw3.db";
+        try {
+            conn = DriverManager.getConnection(url);
+            System.out.println(conn);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public static void disconnect(Connection conn) {
+        if (conn != null){
+            try{
+                conn.close();
+            }catch (SQLException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
 }
