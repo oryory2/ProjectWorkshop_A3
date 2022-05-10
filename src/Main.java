9bb94f8aaefb0b3c8a7a29b54b7cf552356b7c83
Jonathan.P <yonman2@gmail.com>
2022-05-10 16:00:14 +0300

    Removed jar file

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main
         DB_handler.connect_DB();
 //        DB_handler.add_row("login", new String[]{"username", "password"}, new String[]{"yoni.5", "9999"});
         DB_handler.remove_row("login", "username", "yoni.5");
-//        DB_handler.checkLogin("yoni.1","99998");
+        DB_handler.checkLogin("yoni.1","99998");
 //        DB_handler.disconnect_DB();
     }
 }
