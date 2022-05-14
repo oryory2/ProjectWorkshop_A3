4cbcce4bc3bcdeeefd3a1003c624c818c170965e
elbam <elbamit@hotmail.com>
2022-05-14 14:38:48 +0300

    Added the Team and League classes

---
+++ src/Domain/Team.java
@@ -0,0 +1,48 @@
+package Domain;
+
+public class Team {
+
+    private String team_name;
+    private int team_id;
+    private League league;
+    private String field_name;
+
+    public Team(String team_name, int team_id, League league, String field_name) {
+        this.team_name = team_name;
+        this.team_id = team_id;
+        this.league = league;
+        this.field_name = field_name;
+    }
+
+    public String getTeam_name() {
+        return team_name;
+    }
+
+    public void setTeam_name(String team_name) {
+        this.team_name = team_name;
+    }
+
+    public int getTeam_id() {
+        return team_id;
+    }
+
+    public void setTeam_id(int team_id) {
+        this.team_id = team_id;
+    }
+
+    public League getLeague() {
+        return league;
+    }
+
+    public void setLeague(League league) {
+        this.league = league;
+    }
+
+    public String getField_name() {
+        return field_name;
+    }
+
+    public void setField_name(String field_name) {
+        this.field_name = field_name;
+    }
+}
